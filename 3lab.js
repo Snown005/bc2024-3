e8c1b302f327c171a2769592ac6cbf48e4829fb0
Snown005_bc2024-3 <nazardovgan005@gmail.com>
2024-09-29 11:28:38 +0300

    The last main project update

--- 3lab.js
+++ 3lab.js
@@ -1,4 +1,4 @@
-const { program } = require('commander');
+/*const { program } = require('commander');
 const fs = require('fs');
 const path = require('path');
 
@@ -27,19 +27,21 @@ fs.readFile(options.input, 'utf-8', (err, data) => {
         console.log(err);
         process.exit(1);
     }
-
+    const parsedData = JSON.parse(data);
+    const filtere=parsedData.filter((dat=>dat.ku==="13"&&dat.value>5));
+    const result=JSON.stringify(filtere);
     if (options.output && options.display) {
-        fs.writeFile(options.output, data, (err) => {
+        fs.writeFile(options.output, result, (err) => {
             if (err) {
                 console.error(`Помилка при запису у файл: ${err.message}`);
                 process.exit(1);
             }
             console.log(`Результат збережено у файл: ${options.output}`);
-            console.log(data); 
+            console.log(result); 
         });
     } else {
         if (options.output) {
-            fs.writeFile(options.output, data, (err) => {
+            fs.writeFile(options.output, result, (err) => {
                 if (err) {
                     console.error(`Помилка при запису у файл: ${err.message}`);
                     process.exit(1);
@@ -48,8 +50,50 @@ fs.readFile(options.input, 'utf-8', (err, data) => {
             });
         }
         if (options.display) {
-            console.log(data);
+            console.log(result);
         }
     }
-});
+});*/
 
+const { program } = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+program
+    .requiredOption('-i, --input <path>', 'шлях до файлу для читання')
+    .option('-o, --output <path>', 'шлях до файлу для запису результату')
+    .option('-d, --display', 'вивести результат у консоль');
+
+program.parse();
+const options = program.opts();
+
+if (!options.input) {
+    console.error("Please, specify input file");
+    process.exit(1);
+}
+if (!fs.existsSync(options.input)) {
+    console.error("Cannot find input file");
+    process.exit(1);
+}
+if (!options.output && !options.display) {
+    process.exit(1);
+}
+
+const data = fs.readFileSync(options.input, 'utf-8');
+const parsedData = JSON.parse(data);
+const filtered = parsedData.filter(dat => dat.ku === "13" && dat.value > 5);
+const result = JSON.stringify(filtered);
+
+if (options.output && options.display) {
+    fs.writeFileSync(options.output, result);
+    console.log(`Результат збережено у файл: ${options.output}`);
+    console.log(result); 
+} else {
+    if (options.output) {
+        fs.writeFileSync(options.output, result);
+        console.log(`Результат збережено у файл: ${options.output}`);
+    }
+    if (options.display) {
+        console.log(result);
+    }
+}
